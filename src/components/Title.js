e8b31c9a42bce73b4375b60306617e58aec2c13d
jbrasko <jbrasko0508@gmail.com>
2021-04-29 14:22:04 -0600

    add styled-components, update Title to take styled-components

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import styled from 'styled-components/macro'
 
 const Title = () => {
     return (
-        <div className="title">
+        <Wrapper>
             <p>WaveShop</p>
-        </div>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+    position: sticky;
+    top: 0;
+    box-sizing: border-box;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: baseline;
+    background: #c01a1a;
+    font-family: 'Monoton', cursive;
+    color: white;
+    font-size: 300%;
+`
 
 export default Title
